71f4d40cf542a497fd887f87ad0558e09b7935e0
Demétrio Gurgel <98848501+DemetrioGurgel@users.noreply.github.com>
2025-02-24 15:27:09 -0300

    Delete GisaProject directory

--- GisaProject/GisaProject/monitoramento/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import WaterSystem, Device, Measurement, FiscalProfile
-
-admin.site.register(WaterSystem)
-admin.site.register(Device)
-admin.site.register(Measurement)
-admin.site.register(FiscalProfile)
