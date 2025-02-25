320314d708283ac5972eff029f48374c0fe00dc6
Demétrio Gurgel <98848501+DemetrioGurgel@users.noreply.github.com>
2025-02-25 15:27:48 -0300

    Delete GisaProject directory

--- GisaProject/monitoramento/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import dashboard, profile, historico
-
-urlpatterns = [
-    path('', dashboard, name='dashboard'),
-    path('accounts/profile/', profile, name='profile'),
-    path('historico/<str:parameter>/', historico, name='historico'),
-]
